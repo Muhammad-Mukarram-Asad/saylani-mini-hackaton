981718a634f48e2a3699d840df059bd740411ad7
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2022-11-16 15:21:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+function start()
+{
+    Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Lets Begin',
+        showConfirmButton: false,
+        text: "you are ready to go",
+        
+      })
+}
+function f1(){
+Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    }
+  })
+}
+
+function f2()
+{
+    Swal.fire({
+        title: 'Do you want to save the changes?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Save',
+        denyButtonText: `Don't save`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire('Saved!', '', 'success')
+        } else if (result.isDenied) {
+          Swal.fire('Changes are not saved', '', 'info')
+        }
+      })
+}
